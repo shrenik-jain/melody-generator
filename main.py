bbbae5eb99de9d49fd27b133b60e407bdc517e0a
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2021-04-08 12:59:44 +0530

    added notes for happy-birthday tune

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     # music_notes = 'D-B-A-G-D-D-D-D-B-A-G-E--E-C1-B-A-f-D1-D1-C1-A-B--D-B-A-G-D--D-B-A-G-E--E-E-C1-B-A-D1-D1-D1-D1-E1-D1-C1-A-G--D1--B-B-B--B-B-B--B-D1-G-A-B--C1-C1-C1-C1-C1-B-B-B-B-B-A-A-B-A--D1--B-B-B--B-B-B--B-D1-G-A-B--C1-C1-C1-C1-C1-B-B-B-B-D1-D1-C1-G'
     
     # SaReGaMa
-    music_notes = 'C-D-E-F-G-A-B-C1-C1-B-A-G-F-E-D-C'
+    # music_notes = 'C-D-E-F-G-A-B-C1-C1-B-A-G-F-E-D-C'
+    
+    # happy-birthday
+    music_notes = 'C-C-D-C-F-E--C-C-D-C-G-F--C-C-C1-A-G-E-D--a-a-A-F-G-F'
 
     piano = Piano()
     generate = Generator()
@@ -25,9 +28,9 @@ if __name__ == '__main__':
 
     # write('tunes/twinkle-twinkle.wav', sampling_rate, data.astype(np.int16))
     # write('tunes/jingle-bells.wav', sampling_rate, data.astype(np.int16))
-    write('tunes/saregama.wav', sampling_rate, data.astype(np.int16))
+    # write('tunes/saregama.wav', sampling_rate, data.astype(np.int16))
+    write('tunes/happy-birthday.wav', sampling_rate, data.astype(np.int16))
 
-    
     chords = 'EgB-DfA-AcE-BDf-gAcE-fAc'
     data = Piano.fetch_chord_data(sampling_rate, chords)
     data = data * (16300/np.max(data))
